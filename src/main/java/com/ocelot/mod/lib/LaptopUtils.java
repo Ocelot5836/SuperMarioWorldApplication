c09ab3a669e8b582b122e7627fb8d5cba8292015
Ocelot5836 <FinntheRaider@gmail.com>
2018-05-18 15:13:56 -0700

    Removed LaptopUtils.java

---
+++ src/main/java/com/ocelot/mod/lib/LaptopUtils.java
@@ -0,0 +1,20 @@
+package com.ocelot.mod.lib;
+
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
+import net.minecraft.util.ResourceLocation;
+
+public class LaptopUtils {
+
+	private static final BufferedImage SHEET = null;
+	
+	public static void register() {
+		
+	}
+	
+	public static void stitch() {
+		
+	}
+}
