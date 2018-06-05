9cc8f83157773abf48f3293c12eb958b407c31d6
Ocelot5836 <FinntheRaider@gmail.com>
2018-06-05 09:21:17 -0700

    added more colors to color palette

--- src/main/java/com/ocelot/mod/game/core/gfx/ColorPalette.java
+++ src/main/java/com/ocelot/mod/game/core/gfx/ColorPalette.java
@@ -6,14 +6,14 @@ import com.ocelot.mod.lib.Colorizer;
 
 public class ColorPalette {
 
-	public static final ColorPalette TEST = new ColorPalette(0xffff00ff, 0xffffff00, 0xff0000ff, 0xff000000, 0xff00ff00, 0xffffffff, 0xff7f007f, 0xffff7fff);
-	public static final ColorPalette COIN_GOLD = new ColorPalette(0xff000000, 0xffd8a038, 0xfff8d820, 0xfff8f800, 0xffe8f0f8, -1, -1, -1);
-	public static final ColorPalette COIN_BLUE = new ColorPalette(0xff000000, 0xff484888, 0xff6868b0, 0xff8080c8, 0xffe8f0f8, -1, -1, -1);
+	public static final ColorPalette TEST = new ColorPalette(0xffff00ff, 0xffffff00, 0xff0000ff, 0xff000000, 0xff00ff00, 0xffffffff, 0xff7f007f, 0xffff7fff, -1, -1, -1, -1);
+	public static final ColorPalette COIN_GOLD = new ColorPalette(0xff000000, 0xffd8a038, 0xfff8d820, 0xfff8f800, 0xffe8f0f8, -1, -1, -1, -1, -1, -1, -1);
+	public static final ColorPalette COIN_BLUE = new ColorPalette(0xff000000, 0xff484888, 0xff6868b0, 0xff8080c8, 0xffe8f0f8, -1, -1, -1, -1, -1, -1, -1);
 
 	private int[] colors;
 
-	public ColorPalette(int color1, int color2, int color3, int color4, int color5, int color6, int color7, int color8) {
-		this.colors = new int[] { color1, color2, color3, color4, color5, color6, color7, color8 };
+	public ColorPalette(int color1, int color2, int color3, int color4, int color5, int color6, int color7, int color8, int color9, int color10, int color11, int color12) {
+		this.colors = new int[] { color1, color2, color3, color4, color5, color6, color7, color8, color9, color10, color11, color12 };
 	}
 
 	public int[] getColors() {
@@ -50,5 +50,21 @@ public class ColorPalette {
 
 	public int getColor8() {
 		return colors[7];
+	}
+
+	public int getColor9() {
+		return colors[8];
+	}
+
+	public int getColor10() {
+		return colors[9];
+	}
+
+	public int getColor11() {
+		return colors[10];
+	}
+
+	public int getColor12() {
+		return colors[11];
 	}
 }
