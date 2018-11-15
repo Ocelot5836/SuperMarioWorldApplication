dcdc8130542ae8c1c4b2de16dba2037381b01a7d
Ocelot5836 <FinntheRaider@gmail.com>
2018-11-14 21:06:42 -0800

    Create SMWAdminToolsMod.java

---
+++ src/main/java/com/ocelot/mod/game/main/mods/SMWAdminToolsMod.java
@@ -0,0 +1,19 @@
+package com.ocelot.mod.game.main.mods;
+
+import com.ocelot.api.mod.MarioPlugin;
+import com.ocelot.api.mod.SMWMod;
+import com.ocelot.mod.game.GameStateManager;
+
+@SMWMod
+public class SMWAdminToolsMod implements MarioPlugin {
+
+	@Override
+	public void register() {
+		
+	}
+
+	@Override
+	public void registerGameStates(GameStateManager gsm) {
+		
+	}
+}
